0a1b0e3096148db498cdda14a730538820240844
OlehPalka <palka280617@gmail.com>
2022-01-10 19:26:38 +0200

    Initial commit

---
+++ src/main/java/domain/Student.java
@@ -0,0 +1,40 @@
+package domain;
+
+import json.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Andrii_Rodionov on 1/3/2017.
+ */
+public class Student extends BasicStudent {
+    private ArrayList<Tuple<String, Integer>> exams;
+
+    public Student(String name, String surname, Integer year, Tuple<String, Integer>... exams) {
+        super(name, surname, year);
+        this.exams = new ArrayList(Arrays.asList(exams));
+    }
+
+    @Override
+    public JsonObject toJsonObject() {
+        JsonPair name = new JsonPair("name", new JsonString(this.name));
+        JsonPair surname = new JsonPair("surname", new JsonString(this.surname));
+        JsonPair year = new JsonPair("year", new JsonNumber(this.year));
+        JsonObject Stats = new JsonObject(name, surname, year);
+        JsonObject[] ExamsToInsert = new JsonObject[exams.size()];
+        for (int i = 0; i < exams.size(); i++) {
+            Tuple<String, Integer> exam = exams.get(i);
+            JsonObject CurObj = new JsonObject();
+            CurObj.add(new JsonPair("course", new JsonString(exam.key)));
+            CurObj.add(new JsonPair("mark", new JsonNumber(exam.value)));
+            boolean passed = false;
+            if (exam.value > 2) {
+                passed = true;
+            }
+            CurObj.add(new JsonPair("passed", new JsonBoolean(passed)));
+            ExamsToInsert[i] = CurObj;
+        }
+        return new JsonObject(name, surname, year, new JsonPair("exams", new JsonArray(ExamsToInsert)));
+    }
+}
---
+++ src/main/java/json/JsonObject.java
@@ -0,0 +1,49 @@
+package json;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Andrii_Rodionov on 1/3/2017.
+ */
+public class JsonObject extends Json {
+    private Map<String, Json> values = new HashMap<String, Json>();
+
+    public JsonObject(JsonPair... jsonPairs) {
+        for (JsonPair pair: jsonPairs) {
+            values.put(pair.key, pair.value);
+        }
+    }
+
+    @Override
+    public String toJson() {
+        String MainPart = "";
+
+        for (String Key: values.keySet()) {
+            if (!MainPart.equals("")) {
+                MainPart += " ,";
+            }
+            MainPart += "'" + Key + "': ";
+            MainPart += values.get(Key).toJson() ;
+        }
+        return "{" + MainPart + "}";
+    }
+
+    public void add(JsonPair jsonPair) {
+        values.put(jsonPair.key, jsonPair.value);
+    }
+
+    public Json find(String name) {
+        return values.get(name);
+    }
+
+    public JsonObject projection(String... names) {
+        JsonObject proj = new JsonObject();
+
+        for (String name: names) {
+            if (values.containsKey(name)) {
+                proj.add(new JsonPair(name, values.get(name)));
+            }
+        }
+        return proj;
+    }
+}
